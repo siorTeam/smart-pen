3ecd311187c2597b40489a87dd6d6bb32b23debd
whdcks877 <whdcks887@naver.com>
2022-08-09 20:39:19 +0900

    modify Magnetometer calibration

--- week2/whdcks/Oscillatory-Motion-Tracking-With-x-IMU/MagneticCorrection.m
+++ week2/whdcks/Oscillatory-Motion-Tracking-With-x-IMU/MagneticCorrection.m
@@ -1,32 +1,37 @@
 clear
-s = serialport("COM7",115200);
-imudata = zeros([300 9]);
-
+s = serialport("COM12",115200);
+flush(s);
+readline(s);
 f = waitbar(0, "Rotate each axis", 'Name', "Calibrating Magnetometer...");
-for c = 1:300
-    waitbar(c/300,f)
-    imudata(c,:) = cast(split(readline(s), ","),"double");
+tic;
+stopTimer = 10;
+magReadings=[];
+
+while(toc<stopTimer)
+    imudata = cast(split(readline(s), ", "),"double");
+    mag = [imudata(7) imudata(8) imudata(9)];
+    magReadings = [magReadings;mag];
+    waitbar(toc/stopTimer,f)
 end
 delete(f);
-mag = [imudata(:, 7) imudata(:, 8) imudata(:, 9)];
 
 figure("Name", "Magnetometor Calibration");
-plot3(mag(:, 1), mag(:, 2), mag(:,3), "rs")
+plot3(magReadings(:, 1), magReadings(:, 2), magReadings(:,3), "rs")
 hold on
 
-magx_min = min(mag(:,1));
-magx_max = max(mag(:,1));
-magy_min = min(mag(:,2));
-magy_max = max(mag(:,2));
-magz_min = min(mag(:,3));
-magz_max = max(mag(:,3));
+magx_min = min(magReadings(:,1));
+magx_max = max(magReadings(:,1));
+magy_min = min(magReadings(:,2));
+magy_max = max(magReadings(:,2));
+magz_min = min(magReadings(:,3));
+magz_max = max(magReadings(:,3));
 
 
 magx_correction = (magx_max+magx_min)/2;
 magy_correction = (magy_max+magy_min)/2;
 magz_correction = (magz_max+magz_min)/2;
 
-mag_corrected = [mag(:, 1)-magx_correction mag(:, 2)-magy_correction mag(:, 3)-magz_correction];
+mag_corrected = [magReadings(:, 1)-magx_correction magReadings(:, 2)-magy_correction magReadings(:, 3)-magz_correction];
 
 plot3(mag_corrected(:,1), mag_corrected(:,2), mag_corrected(:,3), "bs");
 
